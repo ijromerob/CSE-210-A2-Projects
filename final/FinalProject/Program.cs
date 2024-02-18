5830539290bd607bfd894e3b651b2a9b621946fd
ijromerob <ijromerob@gmail.com>
2024-02-17 23:59:54 -0700

    description added

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        // as of right now the classes that are created are the areas of the hospital
+        //Base Class: Area
+        // Shared Rooms
+        //Private Rooms
+        // Clinic
+        // The clases for the different employees are
+        //Base Class: Employee
+        // Nurses
+        //Doctors
+        // Class for the supplies is created
+        Console.Write("");
     }
 }
